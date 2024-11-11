5510fe83d455315fe46c9099746f3a3ac70a9ee8
mashoun <mashoun.me@gmail.com>
2024-11-11 13:16:14 +0200

    +documented

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 console.log('app.js is working');
+
+// this product class is used to prevent some fallbacks senarios like what if the api for some reason did not return the title property?
+// this way i can always make sure eno the Product obj always gona have a the required props
 class Product {
     constructor(payload = {}) {
-        // never trust eno always there will gonna be an id or an image prop comming the api,
-        // in this senario will consider that the type is always going to be a string
         // this way will be easier to add props and no crash will happen in the design 
         let props = ['id', 'category', 'description', 'image', 'price', 'rating', 'title']
         for (let node of props) {
             this[node] = payload[node] ? payload[node] : ""
             // this way iza shi property mana mawjoude empty string wont make any problems in the design
         }
+        // also 7atta law el url of image exists but also it might be broken!
         this.isBroken = false
     }
 }
@@ -17,32 +19,39 @@ const app = Vue.createApp({
         return {
             title: 'Platzi Store',
             api: 'https://fakestoreapi.com/products',
+            // to spinn when it fetches the data
             spinner: false,
+            // this rep the status of the api if it is working
+            status: true,
             products: [],
         }
     },
     methods: {
         readProducts() {
+            // we store it in a variable because sometimes in future iza bede a3mel query the main value wont be affected
             let api = this.api
             this.spinner = true
-            fetch(api).then(res => {
-                if (res.status === 200 || res.status === 201) return res.json()
-            }).then(res => {
+            fetch(api).then(res => res.json()).then(res => {
                 this.spinner = false
                 this.products = res
                 this.products.map(node => new Product(node))
 
+            }).catch(err => {
+                this.spinner = false
+                console.log(err);
+                this.status = false
             })
         },
         renderImg(obj = {}) {
             return obj.isBroken ? "https://placehold.co/400" : obj.image
         },
-        
+
 
     },
     mounted() {
         this.readProducts()
     }
 })
+
 
 app.mount("#app")
